bd520f478d4741fcb8b889eefcf145a7876d3132
ZhaoShiXiang1 <105416803+ZhaoShiXiang1@users.noreply.github.com>
2023-06-07 20:57:05 +0800

    111

--- open/test.py
+++ open/test.py
@@ -7,14 +7,28 @@ with open(r'D:\Users\PC\Desktop\test\企业.csv',encoding='utf-8') as f:
 # print(df1.columns)
 # print(df2.columns)
 df3=pd.merge(df1,df2,how='left',left_on=['企业名称','ym'],right_on=['企业名','年月'])
-df3=pd.merge(df3,df2,how='inner',left_on=['企业名称'],right_on=['企业名'])
-# print(df3.columns)
-df3.reset_index(drop=False)
-df3=df3[['企业名称','统一信用代号','ym','行业','地市','能耗等级','pq','电(千瓦时)_x','煤(标准煤)_x','油(标准煤)_x','天然气(标准煤)_x','热力(标准煤)_x','综合能耗(标准煤)_x']]
+df3.loc[(df3['天然气(标准煤)'].isnull().T.any()),'是否补数据']='1'
+#df3=pd.merge(df3,df2,how='inner',left_on=['企业名称'],right_on=['企业名'])h
+#包含
+df3=df3[df3['企业名称'].isin(df2['企业名'])]
+
+
+#print(df3.columns)
+#df3.reset_index(drop=False)
+
+
+
 df3=df3.drop_duplicates()
-df3.loc[(df3['电(千瓦时)_x'].isnull().T.any()),'是否补数据']='1'
-df3.to_excel(r'D:\Users\PC\Desktop\test\结果.xlsx', index=False)
-print(df3)
+# print(df3)
+df4=df3.groupby(["企业名称","统一信用代号","ym","行业","地市","电(千瓦时)","煤(标准煤)","油(标准煤)","天然气(标准煤)","热力(标准煤)","综合能耗(标准煤)","是否补数据"],dropna=False,).agg({"能耗等级":"max","pq":"sum"}).reset_index()
+df3=df4[['企业名称','统一信用代号','ym','行业','地市','能耗等级','pq','电(千瓦时)','煤(标准煤)','油(标准煤)','天然气(标准煤)','热力(标准煤)','综合能耗(标准煤)','是否补数据']]
+df3.to_excel(r'D:\Users\PC\Desktop\test\结果3.xlsx', index=False)
+
+#
+# # df3=df3.drop_duplicates()
+# #
+#df4.to_excel(r'D:\Users\PC\Desktop\test\结果.xlsx', index=False)
+# print(df3)
 
 # pd.set_option('display.unicode.ambiguous_as_wide', True)
 # pd.set_option('display.unicode.east_asian_width', True)
