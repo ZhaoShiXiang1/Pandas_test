cbe0f0dbf5bb41baec509e3dc6b8f503ffd4196e
1659850144@qq.com <1659850144@qq.com>
2023-06-07 22:27:56 +0800

    完成

--- open/test.py
+++ open/test.py
@@ -1,16 +1,33 @@
 import pandas as pd
+import os
+os.environ["NUMEXPR_MAX_THREADS"] = "32"
 
 
-df2=pd.read_excel(r'D:\Users\PC\Desktop\test\能耗月数据_20230601.xlsx')
-with open(r'D:\Users\PC\Desktop\test\企业.csv',encoding='utf-8') as f:
+df2=pd.read_excel(r'C:\Users\PC\Desktop\test\能耗月数据_20230601.xlsx')
+with open(r'C:\Users\PC\Desktop\test\企业.csv',encoding='utf-8') as f:
     df1=pd.read_csv(f)
 # print(df1.columns)
 # print(df2.columns)
 df3=pd.merge(df1,df2,how='left',left_on=['企业名称','ym'],right_on=['企业名','年月'])
-df3.loc[(df3['天然气(标准煤)'].isnull().T.any()),'是否补数据']='1'
+
+df3.loc[(df3["综合能耗(标准煤)"].isnull()),'是否补数据']='1'
+df3=df3[df3['企业名称'].isin(df2['企业名'])]
+df3=df3.drop_duplicates()
+df4=df3.groupby(["企业名称","统一信用代号","ym","地市","电(千瓦时)","煤(标准煤)","油(标准煤)","天然气(标准煤)","热力(标准煤)","综合能耗(标准煤)","是否补数据"],dropna=False,).agg({"能耗等级":"max","行业":"max","pq":"sum"}).reset_index()
+df3=df4[['企业名称','统一信用代号','ym','行业','地市','能耗等级','pq','电(千瓦时)','煤(标准煤)','油(标准煤)','天然气(标准煤)','热力(标准煤)','综合能耗(标准煤)','是否补数据']]
+df3.to_excel(r'C:\Users\PC\Desktop\test\结果.xlsx', index=False)
+
+
+
+
+
+
+
+
+
 #df3=pd.merge(df3,df2,how='inner',left_on=['企业名称'],right_on=['企业名'])h
 #包含
-df3=df3[df3['企业名称'].isin(df2['企业名'])]
+
 
 
 #print(df3.columns)
@@ -18,11 +35,9 @@ df3=df3[df3['企业名称'].isin(df2['企业名'])]
 
 
 
-df3=df3.drop_duplicates()
+
 # print(df3)
-df4=df3.groupby(["企业名称","统一信用代号","ym","行业","地市","电(千瓦时)","煤(标准煤)","油(标准煤)","天然气(标准煤)","热力(标准煤)","综合能耗(标准煤)","是否补数据"],dropna=False,).agg({"能耗等级":"max","pq":"sum"}).reset_index()
-df3=df4[['企业名称','统一信用代号','ym','行业','地市','能耗等级','pq','电(千瓦时)','煤(标准煤)','油(标准煤)','天然气(标准煤)','热力(标准煤)','综合能耗(标准煤)','是否补数据']]
-df3.to_excel(r'D:\Users\PC\Desktop\test\结果3.xlsx', index=False)
+
 
 #
 # # df3=df3.drop_duplicates()
